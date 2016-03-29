b9016d0782f981e4811a48e4f12231040be098f8
Vincent Klaiber <vincentklaiber@gmail.com>
2016-03-29 14:45:06 +0200

    Remove errors from alert view

--- resources/views/alert.blade.php
+++ resources/views/alert.blade.php
@@ -7,13 +7,3 @@
         {!! session()->get('alert.message') !!}
     </div>
 @endif
-
-@if ($errors->any())
-    <div class="alert alert-danger alert-dismissible fade in" role="alert">
-        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-            <span aria-hidden="true">&times;</span>
-        </button>
-
-        {{ $errors->first() }}
-    </div>
-@endif
