a28d057bc253cdb032bdc7fed77aad08d7be98bc
Sergio Brighenti <sergio@brighenti.me>
2017-02-24 17:09:30 +0100

    different views for bootstrap 3 and 4
    The view bootstrap3.blade.php contains classes for correct fade out in
    bootstrap 3, and bootstrap4.blade.php contains correct classes for
    bootstrap 3.
    Updated readme with example.

---
+++ resources/views/bootstrap3.blade.php
@@ -0,0 +1,9 @@
+@if (session()->has('alert.message'))
+    <div class="alert alert-{{ session()->get('alert.style') }} alert-dismissible fade in" role="alert">
+        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+        </button><!-- /close -->
+
+        {!! session()->get('alert.message') !!}
+    </div><!-- /alert -->
+@endif
